6df05239640d1e575177dd7bed9c9db18363d33f
Hernan Provoste <hernanprovostev@outlook.com>
2023-08-10 09:26:06 -0400

    add menu item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 0).getTheme(),
+        home: const HomeScreen());
   }
 }
